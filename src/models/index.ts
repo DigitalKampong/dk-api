666fbbae0fb63faae7c7fe5256630e529d046c71
Ivan Tan <ivantjh97@gmail.com>
2020-10-20 00:11:18 +0800

    Dynamically load models in models/index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,17 @@
-import sequelize from '../db';
+import fs from 'fs';
+import path from 'path';
 
-// Model associations should be done here
+const basename = path.basename(__filename);
+const models = {};
+
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return file.indexOf('.') !== 0 && file !== basename;
+  })
+  .forEach(file => {
+    const clazzName = file.substring(0, file.length - 3);
+    const model = require(path.join(__dirname, clazzName)); // this is the class
+    models[model.default.name] = model.default; // we export default from our models
+  });
+
+export default models;

d594ff4332231ddefa95cc92c7b8cc6103eef429
Ivan Tan <ivantjh97@gmail.com>
2020-10-21 21:04:39 +0800

    Fix typescript error

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import {Request, Response, NextFunction} from 'express';
 import cors from 'cors';
 
 // import {testAuthenticate} from './db/dbUtil';
@@ -16,15 +17,15 @@ const PORT = process.env.PORT || '3000';
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
 app.use('/stores', stores);
 
-app.all('*', (req, res) => res.send('You are at the wrong place. Shoo!'));
+app.all('*', (req: Request, res: Response) => res.send('You are at the wrong place. Shoo!'));
 
-app.use((err, req, res, _next) => {
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
   console.error(err.stack);
   res.status(500).send('Something broke! Please try again later.');
 });

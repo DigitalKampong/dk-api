5d5162291ea48b88bef8bd40e5d5caca30f38dc6
Ivan Tan <ivantjh97@gmail.com>
2020-10-31 22:17:32 +0800

    Add sample tests

--- tests/controllers/stallController.test.ts
+++ tests/controllers/stallController.test.ts
@@ -1,15 +1,54 @@
 import request from 'supertest';
+import HawkerCentre from '../../src/models/HawkerCentre';
+import Stall from '../../src/models/Stall';
 import app from '../../src/server';
 
 describe('POST /stalls', () => {
-  it('creates a new stall', async done => {
+  it('returns a new stall', async () => {
     const res = await request(app).post('/stalls').send({name: 'best chicken rice'});
+
     expect(res.status).toEqual(201);
     expect(res.body).toHaveProperty('name', 'best chicken rice');
-    done();
+  });
+
+  it('returns an error given invalid attributes', async () => {
+    const res = await request(app).post('/stalls').send({name: ''});
+
+    expect(res.status).toEqual(400);
   });
 });
 
 describe('GET /stall/:id', () => {
+  it('returns an existing store', async () => {
+    const hcAttr = {name: 'fake name', address: 'fake addr'};
+    const hawkerCentre = await HawkerCentre.create(hcAttr);
+    const stall = await Stall.create({name: 'duck rice', hawkerCentreId: hawkerCentre.id});
+    const res = await request(app).get(`/stalls/${stall.id}`);
 
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('name', 'duck rice');
+    expect(res.body).toHaveProperty('HawkerCentre', hcAttr);
+  });
+});
+
+describe('PUT /stall/:id', () => {
+  it('returns an updated store', async () => {
+    const hcAttr = {name: 'fake name', address: 'fake addr'};
+    const hawkerCentre = await HawkerCentre.create(hcAttr);
+    const stall = await Stall.create({name: 'duck rice', hawkerCentreId: hawkerCentre.id});
+    const res = await request(app).put(`/stalls/${stall.id}`).send({name: 'noodles'});
+
+    expect(res.status).toEqual(200);
+    expect(res.body).toHaveProperty('name', 'noodles');
+  });
+});
+
+describe('DELETE /stall/:id', () => {
+  it('returns success on successful deletion', async () => {
+    const stall = await Stall.create({name: 'duck rice'});
+    const res = await request(app).delete(`/stalls/${stall.id}`);
+
+    expect(res.status).toEqual(200);
+    expect(await Stall.findByPk(stall.id)).toBeNull();
+  });
 })

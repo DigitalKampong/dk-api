338e978524d931f8deade9bcdf095fd06e1cccaa
Ivan Tan <ivantjh97@gmail.com>
2020-10-31 15:20:01 +0800

    wip

--- tests/globalTeardown.ts
+++ tests/globalTeardown.ts
@@ -2,9 +2,9 @@ import { doesNotMatch } from 'assert';
 import sequelize from '../src/db';
 
 export default async function teardown() {
-  try {
-    await sequelize.close();
-  } catch (err) {
-    console.error('Sequelize unable to close:', err);
-  }
+//   try {
+//     await sequelize.close();
+//   } catch (err) {
+//     console.error('Sequelize unable to close:', err);
+//   }
 }

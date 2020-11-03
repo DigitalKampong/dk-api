0f215076b4663cbef0cca6b646bfcf02a2c3b611
Ivan Tan <ivantjh97@gmail.com>
2020-11-03 21:54:37 +0800

    Fix model require bug after build

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,10 +7,16 @@ const models: {[Key: string]: Model} = {};
 
 fs.readdirSync(__dirname)
   .filter(file => {
-    return file.indexOf('.') !== 0 && file !== basename;
+    return (
+      // exclude out build artifacts
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      !file.endsWith('d.ts') &&
+      !file.endsWith('.map')
+    );
   })
   .forEach(file => {
-    const clazzName = file.substring(0, file.length - 3);
+    const clazzName = file.substring(0, file.length - 3); // remove .ts or .js from the back
     const model = require(path.join(__dirname, clazzName)); // this is the class
     models[model.default.name] = model.default; // we export default from our models
   });

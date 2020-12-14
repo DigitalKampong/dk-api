ed9b17c2af85115963dcfd09f2ccff57c9032e7a
wenhaogoh <52305097+wenhaogoh@users.noreply.github.com>
2020-12-14 18:55:02 +0800

    Create search product route (#19)
    * Add search column to products
    
    * Add search route
    
    * Increase search flexibility
    
    * Update documentation
    
    * Expand search region

---
+++ src/controllers/searchController.ts
@@ -0,0 +1,52 @@
+import {Request, Response, NextFunction} from 'express';
+import Stall from '../models/Stall';
+
+async function searchStalls(req: Request, res: Response, next: NextFunction) {
+  try {
+    const query = cleanInput(req.params.query);
+    const stalls = await Stall.sequelize!.query(
+      `
+        SELECT *
+        FROM "Stalls"
+        WHERE id IN (
+          SELECT "stallId"
+          FROM "Products"
+          WHERE _search @@ to_tsquery('english', '${query}')
+        ) OR id IN (
+          SELECT id
+          FROM "Stalls"
+          WHERE _search @@ to_tsquery('english', '${query}')
+        ) OR "hawkerCentreId" IN (
+          SELECT id
+          FROM "HawkerCentres"
+          WHERE _search @@ to_tsquery('english', '${query}')
+        ) OR "hawkerCentreId" IN (
+          SELECT id
+          FROM "HawkerCentres"
+          WHERE "regionId" IN (
+            SELECT id
+            FROM "Regions"
+            WHERE _search @@ to_tsquery('english', '${query}')
+          )
+        )
+      `,
+      {
+        model: Stall,
+        replacements: {query: query},
+      }
+    );
+    res.status(200).json(stalls);
+  } catch (err) {
+    next(err);
+  }
+}
+
+/**
+ * Removes boolean operators from user query for to_tsquery.
+ * @param input User query.
+ */
+function cleanInput(input: String) {
+  return input.replace(/[|&!<>]+/g, '').replace(/ /g, '|');
+}
+
+export const searchFuncs = [searchStalls];
---
+++ src/routes/search.ts
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+import * as searchController from '../controllers/searchController';
+
+const router = Router();
+
+router.get('/:query', searchController.searchFuncs);
+
+export default router;

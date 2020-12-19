1ba3ddc61ca7687b9ed506a413d03c8eeea21a2b
Ivan Tan <ivantjh97@gmail.com>
2020-12-19 16:38:13 +0800

    Fix user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,10 +2,21 @@ import { Model, DataTypes } from 'sequelize';
 
 import sequelize from '../db';
 
-class User extends Model {
+interface UserAttributes {
+  id: number;
+  email: string;
+  password: string;
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
+
+class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
   public id!: number;
   public email!: string;
   public password!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
 User.init(
@@ -14,13 +25,16 @@ User.init(
       type: DataTypes.INTEGER,
       autoIncrement: true,
       primaryKey: true,
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
       unique: true,
+      allowNull: false,
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
   },
   { sequelize }

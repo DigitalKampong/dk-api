ca8622fdf976e5583abdf36d4a365f3ad516114e
wenhaogoh <52305097+wenhaogoh@users.noreply.github.com>
2021-01-07 01:14:01 +0800

    Include categories into search route (#37)
    * Modify search results
    
    * Fix lint
    
    * Include category into search
    
    * Remove CategoryStall associations from Category and Stall
    
    * Update HawkerCentres.csv
    
    * Fix lint warning

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import * as searchController from '../controllers/searchController';
+import * as stallController from '../controllers/stallController';
 
 const router = Router();
 
+router.get('/', stallController.indexStallFuncs);
 router.get('/:query', searchController.searchFuncs);
 
 export default router;

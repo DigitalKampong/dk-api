d2afbbf640de284c91f3bcc5501690ec8968edcd
zatkiller <gheedylan@yahoo.com.sg>
2021-01-19 19:33:55 +0800

    Fixed error

--- src/middleware/adminAuth.ts
+++ src/middleware/adminAuth.ts
@@ -1,5 +1,7 @@
 import { UnauthorizedError } from '../errors/httpErrors';
 
+import { UnauthorizedError } from '../errors/httpErrors';
+
 const ADMIN = {
   email: 'test@example.com',
   password: 'password',
@@ -7,7 +9,7 @@ const ADMIN = {
 
 const adminAuth = (email: string, password: string) => {
   if (ADMIN.password !== password || ADMIN.email !== email)
-    throw UnauthorizedError('Invalid Admin Credentials!');
+    throw new UnauthorizedError('Invalid Admin Credentials!');
 
   return ADMIN;
 };

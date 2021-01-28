50594f63bea8f3bfe5c5700cda0fb32698ed83f8
zatkiller <gheedylan@yahoo.com.sg>
2021-01-28 22:41:55 +0800

    Linked controllers to add images for hawker

--- src/controllers/hawkerCentreController.ts
+++ src/controllers/hawkerCentreController.ts
@@ -1,6 +1,10 @@
 import { HawkerCentre, Stall } from '../models';
 import { Request, Response, NextFunction } from 'express';
-import { NotFoundError } from '../errors/httpErrors';
+import { BadRequestError, NotFoundError } from '../errors/httpErrors';
+
+import { upload, uploadFormImgs, createImages, destroyImageIds } from './imageController';
+import { MAX_NUM_IMAGES, UPLOAD_FORM_FIELD } from '../consts';
+import sequelize from '../db';
 
 async function retrieveHawkerCentre(req: Request, res: Response, next: NextFunction) {
   try {
@@ -79,8 +83,58 @@ async function destroyHawkerCentre(req: Request, res: Response, next: NextFuncti
   }
 }
 
+async function uploadHawkerCentreImages(req: Request, res: Response, next: NextFunction) {
+  try {
+    const hawkerCentre = req.hawkerCentre!;
+
+    await sequelize.transaction(async t => {
+      const images = await createImages(req.fileNames!, { transaction: t });
+      await hawkerCentre.addImages(images, { transaction: t });
+    });
+
+    await hawkerCentre.reload({
+      include: [
+        {
+          association: HawkerCentre.associations.Stalls,
+          include: [
+            {
+              association: Stall.associations.Images,
+              attributes: ['id', 'downloadUrl'],
+            },
+          ],
+        },
+        { association: HawkerCentre.associations.Images, attributes: ['id', 'downloadUrl'] },
+      ],
+    });
+    res.status(200).json(hawkerCentre);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function destroyHawkerCentreImages(req: Request, res: Response, next: NextFunction) {
+  try {
+    const imageIds = req.body['imageIds'];
+    if (!Array.isArray(imageIds)) {
+      throw new BadRequestError('imageIds key not found in body or not an array');
+    }
+
+    await destroyImageIds(imageIds as number[]);
+    res.status(200).end();
+  } catch (err) {
+    next(err);
+  }
+}
+
 export const indexHawkerCentreFuncs = [indexHawkerCentre];
 export const showHawkerCentreFuncs = [retrieveHawkerCentre, showHawkerCentre];
 export const createHawkerCentreFuncs = [createHawkerCentre];
 export const updateHawkerCentreFuncs = [retrieveHawkerCentre, updateHawkerCentre];
 export const destroyHawkerCentreFuncs = [retrieveHawkerCentre, destroyHawkerCentre];
+export const uploadHawkerCentreImagesFuncs = [
+  retrieveHawkerCentre,
+  upload.array(UPLOAD_FORM_FIELD, MAX_NUM_IMAGES),
+  uploadFormImgs,
+  uploadHawkerCentreImages,
+];
+export const destroyHawkerCentreImagesFuncs = [destroyHawkerCentreImages];
--- src/routes/hawkerCentres.ts
+++ src/routes/hawkerCentres.ts
@@ -9,5 +9,7 @@ router.get('/:id', hawkerCentreController.showHawkerCentreFuncs);
 router.post('/', auth, hawkerCentreController.createHawkerCentreFuncs);
 router.put('/:id', auth, hawkerCentreController.updateHawkerCentreFuncs);
 router.delete('/:id', auth, hawkerCentreController.destroyHawkerCentreFuncs);
+router.post('/:id/upload', auth, hawkerCentreController.uploadHawkerCentreImagesFuncs);
+router.post('/:id/delete-images', auth, hawkerCentreController.destroyHawkerCentreImagesFuncs);
 
 export default router;

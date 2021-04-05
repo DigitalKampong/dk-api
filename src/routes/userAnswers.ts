ddea4598534de015177f202b3a8bb2326f769e1c
Dylan Ghee <59414467+zatkiller@users.noreply.github.com>
2021-04-05 13:05:53 +0800

    Switch Question Answer Creation to be done during Register (#68)
    * Switch Question creation to register
    
    * Fix linting
    
    * Fix doc naming

--- src/routes/userAnswers.ts
+++ src/routes/userAnswers.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import * as userAnswerController from '../controllers/userAnswerController';
-import { auth, adminAuth } from '../middleware/auth';
+import { adminAuth } from '../middleware/auth';
 
 const router = Router();
 
-router.post('/', auth, userAnswerController.createUserAnswerFuncs);
 router.post('/validate', userAnswerController.validateUserAnswerFuncs);
 router.delete('/:id', adminAuth, userAnswerController.destroyUserAnswerFuncs);
 
